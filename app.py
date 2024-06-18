721ea4e08f1ae22fe08675f75f78662d21acac86
jcsoriano01 <jcsoriano.01@gmail.com>
2024-06-18 10:31:22 +0800

    app py enhancement

--- app.py
+++ app.py
@@ -4,24 +4,24 @@ from dash import html
 import dash_bootstrap_components as dbc
 import logging
 
-#Application obhect (stored in app variable), along with CSS stylesheets
-app = dash.Dash(__name__, external_stylesheets=["assets/bootstrap.css"])
+# Application object (stored in app variable), along with CSS stylesheets
+app = dash.Dash(__name__, external_stylesheets=["/assets/bootstrap.css"])
 
 # Expose the server attribute for Gunicorn
 server = app.server
 
-#Supress callbacks when input elements enter the layout
+# Suppress callbacks when input elements enter the layout
 app.config.suppress_callback_exceptions = True
 
-#Get CSS from a local folder
+# Get CSS from a local folder
 app.css.config.serve_locally = True
 
-#Enables your app to run offline
+# Enables your app to run offline
 app.scripts.config.serve_locally = True
 
-#Set app title that appears in your browser tab
+# Set app title that appears in your browser tab
 app.title = 'EZPark'
 
-#The two lines below reduce logs on the terminal to help us debug better
+# The two lines below reduce logs on the terminal to help us debug better
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
